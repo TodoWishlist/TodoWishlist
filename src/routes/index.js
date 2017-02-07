41a3e60ee5d910bc1894d8216a81472438bc4289
Qiyu Li <QiyuLi@users.noreply.github.com>
2017-02-07 15:09:29 -0500

    Introduce profile page

--- src/routes/index.js
+++ src/routes/index.js
@@ -18,6 +18,7 @@ export default {
   children: [
     require('./home').default,
     require('./marketplace').default,
+    require('./profile').default,
     require('./contact').default,
     require('./login').default,
     require('./register').default,
