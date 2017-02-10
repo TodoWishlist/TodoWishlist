1127982d20305cd89a75a876bce21625e4fdfd53
Jiazhen Zhu <jason.jz.zhu@gmail.com>
2017-02-09 21:41:41 -0500

    improve footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,12 +2,12 @@ import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import styles from './Footer.css';
 import FooterGeneral from './components/FooterGeneral/FooterGeneral';
-import FooterNav from './components/FooterNavs/FooterNavs';
+import FooterNavs from './components/FooterNavs/FooterNavs';
 
 const Footer = () => (
   <section className={styles.footerWrapper}>
     <footer className={styles.rowContent}>
-      <FooterNav />
+      <FooterNavs />
       <FooterGeneral />
     </footer>
   </section>
--- src/components/Footer/components/FooterNavs/FooterNavs.js
+++ src/components/Footer/components/FooterNavs/FooterNavs.js
@@ -6,25 +6,47 @@ class FooterNavs extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dataSet: [],
+      dataSet: [{},{},{}],
     };
   }
 
   componentWillMount() {
-
+    this.setState({ dataSet:
+      [
+        {name: 'Company', list: [{name: 'About us', url: '/aboutus'},{name: 'Jobs', url: '/jobs'},{name: 'Blog', url: '/blog'}]},
+        {name: 'Support', list: [{name: 'Help Center', url: '/helpcenter'},{name: 'Contact us', url: '/cotactus'}]},
+        {name: 'Connect', list: [{name: 'Twitter', url: 'www.twitter.com'},{name: 'Facebook', url: 'www.facebook.com'},{name: 'Linkedin', url: 'www.linkedin.com'},{name: 'Google+', url: 'www.google.com'}]}
+      ],
+    }
+  )
   }
 
   render() {
     return (
       <div className={styles.FooterNavWrapper}>
-        <FooterNav />
+        {
+          this.state.dataSet.map((data, index) =>
+            <FooterNav key={index} dataSet={data} />
+          )
+        }
       </div>
     );
   }
 }
 
-const FooterNav = () => (
-  <div>Nav</div>
+const FooterNav = ({ dataSet }) => (
+  <div className={styles.FooterNavSection}>
+    <p>{dataSet.name}</p>
+    <ul className={styles.Links}>
+      {
+        dataSet.list.map((data, index) =>
+          <li key={index}>
+            <a className={styles.href} href={data.url}>{data.name}</a>
+          </li>
+        )
+      }
+    </ul>
+  </div>
 );
 
 export default withStyles(styles)(FooterNavs);
