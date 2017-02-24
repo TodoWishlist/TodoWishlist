559be09e644e35263ee35fadc242a9d5fb11efb5
Qiyu Li <QiyuLi@users.noreply.github.com>
2017-02-24 14:32:02 -0500

    Fix typo

--- src/components/Html.js
+++ src/components/Html.js
@@ -42,7 +42,7 @@ class Html extends React.Component {
           <meta name="description" content={description} />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="apple-touch-icon" href="apple-touch-icon.png" />
-	  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+          <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
           <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" />
           {styles.map(style =>
             <style
